cbf2ccdc4e54424b1cc73a29e946af73c78efbf0
pozid <pozidaev2002@gmail.com>
2023-04-20 00:37:45 +0500

    Сделал 1 задание

---
+++ 1.py
@@ -0,0 +1,7 @@
+date = "20"
+month = "Апреля"
+temperature = -12
+if temperature < 0:
+    print("Холодно, лучше остаться дома")
+else:
+    print("Сегодня " + date + ' ' + month + ". На улице " + str(temperature) + " градусов.")
